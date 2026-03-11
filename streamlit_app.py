a297908754bb515f7ab0c82e452cd2459242edd6
mlopezfb <marta.lopezfb@sdggroup.com>
2026-03-11 17:20:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,10 @@ if ingredients_list:
         
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
+      
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+        session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
   
     
